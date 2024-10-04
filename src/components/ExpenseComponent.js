5f36e1d735dd95d7d9395225ffb72432c5607350
Brandon Lew <brandonlew2000@gmail.com>
2024-10-04 16:29:07 -0400

    added sorting and filtering

--- src/components/ExpenseComponent.js
+++ src/components/ExpenseComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ExpenseService from '../services/ExpenseService';
-import { useNavigate } from 'react-router-dom';
+import { Select, MenuItem, ListItemText, ListItemIcon, Checkbox } from '@mui/material';
+import CategoryService from '../services/CategoryService';
+import SubcategoryService from '../services/SubcategoryService';
 
 class ExpenseComponent extends React.Component {
 
@@ -14,6 +16,10 @@ class ExpenseComponent extends React.Component {
             beforeDate: "",
             afterDate: "",
             total: 0,
+            filteredCategories: [],
+            filteredSubcategories:[],
+            sort: "date",
+            sortDirection: 1
         }
     }
 
@@ -33,6 +39,16 @@ class ExpenseComponent extends React.Component {
         ExpenseService.getExpenses().then((expenseResponse) => {
             this.setState({expenses: expenseResponse.data})
         });
+
+        CategoryService.getCategories().then((categoryResponse) => {
+            this.setState({categories: categoryResponse.data})
+            this.setState({filteredCategories: categoryResponse.data})
+        })
+
+        SubcategoryService.getSubcategories().then((subcategoryResponse) => {
+            this.setState({subcategories: subcategoryResponse.data})
+            // this.setState({filteredSubcategories: subcategoryResponse.data})
+        })
 
         let today = new Date();
         this.setState({beforeDate: this.formatJsDateToNormalDate(today)});
@@ -55,16 +71,50 @@ class ExpenseComponent extends React.Component {
     }
 
     getFilteredExpenses = () => {
-        const { expenses, searchTerm, afterDate, beforeDate } = this.state;
+        const { expenses, searchTerm, afterDate, beforeDate, filteredCategories, filteredSubcategories, sort, sortDirection } = this.state;
 
-        return expenses.filter(expense => {
+        let filtered = expenses.filter(expense => {
             const search = expense.note.toLowerCase().includes(searchTerm.toLowerCase())
 
             const expenseDate = new Date(expense.date);
             const inDateRange =(!afterDate || new Date(afterDate) <= expenseDate) && (!beforeDate || new Date(beforeDate) >= expenseDate);
 
-            return search && inDateRange;
+            let category = false;
+            for (let i = 0; i < filteredCategories.length; i++) {
+                if (filteredCategories[i].categoryName == expense.category.categoryName) {
+                    category = true;
+                }
+            }
+
+            // let subcategory = false;
+            // console.log(filteredSubcategories)
+            // for (let i = 0; i < filteredSubcategories.length; i++) {
+            //     if (filteredSubcategories[i].subcategoryName == expense.subcategory.subcategoryName) {
+            //         subcategory = true;
+            //     }
+            // }
+
+            return search && inDateRange && category;
         } );
+        switch (sort) {
+            case ("date"):
+                return filtered.sort(function(a,b) {
+                    return (new Date(b.date) - new Date(a.date)) * sortDirection;
+                })
+            case ("amount"):
+                return filtered.sort(function(a,b) {
+                    return (b.amount - a.amount) * sortDirection;
+                })
+            case ("category"):
+                return filtered.sort(function(a,b) {
+                    return (b.category.categoryName < a.category.categoryName) * sortDirection;
+                })
+            case ("subcategory"):
+                return filtered.sort(function(a,b) {
+                    return (b.subcategory.subcategoryName < a.subcategory.subcategoryName) * sortDirection;
+                })
+        }
+        
     }
 
     getTotal = (filteredExpenses) => {
@@ -77,14 +127,76 @@ class ExpenseComponent extends React.Component {
         return sum;
     }
 
-    addExpense () {
-        const navigate = useNavigate();
-        navigate('/add');
+    filterSubcategory = () => {
+        const { filteredCategories, subcategories } = this.state;
+        if (!filteredCategories) return [];
+
+        return subcategories.filter(subcategory => {
+            for (let i = 0; i < filteredCategories.length; i++) {
+                if (filteredCategories[i].id == subcategory.category)
+                    return true;
+            }
+            return false;
+        });
     }
+
+    sortDate = () => {
+        if (this.state.sort == "date") {
+            this.setState({sortDirection: this.state.sortDirection * -1})
+        } else {
+            this.setState({sortDirection: 1});
+            this.setState({sort: "date"});
+        }
+        
+    }
+
+    sortCategory = () => {
+        if (this.state.sort == "category") {
+            this.setState({sortDirection: this.state.sortDirection * -1})
+        } else {
+            this.setState({sortDirection: 1});
+            this.setState({sort: "category"});
+        }
+    }
+
+    sortSubcategory = () => {
+        if (this.state.sort == "subcategory") {
+            this.setState({sortDirection: this.state.sortDirection * -1})
+        } else {
+            this.setState({sortDirection: 1});
+            this.setState({sort: "subcategory"});
+        }
+    }
+
+    sortNote = () => {
+        if (this.state.sort == "note") {
+            this.setState({sortDirection: this.state.sortDirection * -1})
+        } else {
+            this.setState({sortDirection: 1});
+            this.setState({sort: "note"});
+        }
+    }
+
+    sortAmount = () => {
+        if (this.state.sort == "amount") {
+            this.setState({sortDirection: this.state.sortDirection * -1})
+        } else {
+            this.setState({sortDirection: 1});
+            this.setState({sort: "amount"});
+        }
+    }
+
+    changeCategory = (event) => {
+        this.setState({filteredCategories: event.target.value})
+    }
+    // changeSubcategory = (event) => {
+    //     this.setState({filteredSubcategories: event.target.value})
+    // }
     
     render () {
         const filteredExpenses = this.getFilteredExpenses();
         const total = this.getTotal(filteredExpenses);
+        // let filteredSubcategories = this.filterSubcategory();
         
         return (
             <div>
@@ -96,33 +208,57 @@ class ExpenseComponent extends React.Component {
                     <input type="date" className="datepicker" value={this.state.afterDate} onChange={this.changeAfterDate}/>
                     <label>And</label>
                     <input type="date" className="datepicker" value={this.state.beforeDate} onChange={this.changeBeforeDate}/>
+
+                    <label>Category</label>
+                    <Select value={this.state.filteredCategories} multiple className='dropdown' onChange={this.changeCategory} renderValue={(selected)=>{selected.join(' ')}}>
+                    {this.state.categories.map(category => 
+                        <MenuItem key={category.id} value={category}>
+                            <ListItemIcon>
+                                <Checkbox checked={this.state.filteredCategories.includes(category)}></Checkbox>
+                            </ListItemIcon>
+                            <ListItemText primary={category.categoryName}></ListItemText>
+                        </MenuItem>
+                    )}
+                    </Select>
+
+                    {/* <label>Subcategory</label>
+                    <Select value={this.state.filteredSubcategories} multiple className='dropdown' onChange={this.changeSubcategory} renderValue={(selected)=>{selected.join(' ')}}>
+                    {filteredSubcategories.map(subcategory => 
+                        <MenuItem key={subcategory.id} value={subcategory}>
+                            <ListItemIcon>
+                                <Checkbox checked={this.state.filteredSubcategories.includes(subcategory)}></Checkbox>
+                            </ListItemIcon>
+                            <ListItemText primary={subcategory.subcategoryName}></ListItemText>
+                        </MenuItem>
+                    )}
+                    </Select> */}
                 </nav>
                 
                 <table className='table table-striped'>
                     <thead>
                         <tr>
-                            <td>Date</td>
-                            <td>Amount</td>
+                            <td onClick={this.sortDate}>Date</td>
+                            <td onClick={this.sortCategory}>Category</td>
+                            <td onClick={this.sortSubcategory}>Subcategory</td>
                             <td>Note</td>
-                            <td>Category</td>
-                            <td>Subcategory</td>
+                            <td className='amount' onClick={this.sortAmount}>Amount</td>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             filteredExpenses.map(expense => 
                                 <tr key = {expense.id.timestamp}>
-                                    <td>{(new Date(expense.date)).getFullYear()}-{(new Date(expense.date)).getMonth() + 1}-{(new Date(expense.date)).getDate() + 1}</td>
-                                    <td>{expense.amount}</td>
-                                    <td>{expense.note}</td>
+                                    <td>{this.formatJsDateToNormalDate(new Date(expense.date))}</td>
                                     <td>{expense.category.categoryName}</td>
                                     <td>{expense.subcategory.subcategoryName}</td>
+                                    <td>{expense.note}</td>
+                                    <td className='amount'>{expense.amount}</td>
                                 </tr>
                             )
                         }
                     </tbody>
                 </table>
-                <div>Total: {total}</div>
+                <div className='amount'>Total: {total}</div>
             </div>
         )
     }
