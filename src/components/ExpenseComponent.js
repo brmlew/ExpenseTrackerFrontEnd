89c6a73ed6b4cbd27a5142ea04679a7249f98dfc
Brandon Lew <brandonlew2000@gmail.com>
2024-10-07 13:33:24 -0400

    added sorting icon

--- src/components/ExpenseComponent.js
+++ src/components/ExpenseComponent.js
@@ -213,11 +213,27 @@ class ExpenseComponent extends React.Component {
                 <table className='table table-striped'>
                     <thead>
                         <tr>
-                            <td onClick={this.sortDate}>Date</td>
-                            <td onClick={this.sortCategory}>Category</td>
-                            <td onClick={this.sortSubcategory}>Subcategory</td>
+                            <td className='header' onClick={this.sortDate}>Date
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-vertical" viewBox="0 0 16 16">
+                                    <path d="M8.354 14.854a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 13.293V2.707L6.354 3.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 2.707v10.586l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                </svg>
+                            </td>
+                            <td className='header' onClick={this.sortCategory}>Category
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-vertical" viewBox="0 0 16 16">
+                                    <path d="M8.354 14.854a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 13.293V2.707L6.354 3.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 2.707v10.586l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                </svg>
+                            </td>
+                            <td className='header' onClick={this.sortSubcategory}>Subcategory
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-vertical" viewBox="0 0 16 16">
+                                    <path d="M8.354 14.854a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 13.293V2.707L6.354 3.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 2.707v10.586l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                </svg>
+                            </td>
                             <td>Note</td>
-                            <td className='amount' onClick={this.sortAmount}>Amount</td>
+                            <td className='amount header' onClick={this.sortAmount}>Amount
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-vertical" viewBox="0 0 16 16">
+                                    <path d="M8.354 14.854a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 13.293V2.707L6.354 3.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 2.707v10.586l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                </svg>  
+                            </td>
                         </tr>
                     </thead>
                     <tbody>
